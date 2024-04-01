4f1adaebfc7a7e173c6b5525bd2c12a057c7c19d
Jonathan Aguilar <87777517+JonathanAguilar358@users.noreply.github.com>
2024-04-01 15:10:45 -0400

    Delete js directory

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger')
-const dropdownMenu = document.querySelector('.dropdown_menu')
-
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('is-active');
-    dropdownMenu.classList.toggle('is-active')
-} )
