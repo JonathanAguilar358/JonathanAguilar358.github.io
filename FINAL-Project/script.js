16a48382ca62a56c2abdfd19852af3cfa7ec306a
Jonathan Aguilar <jonathan.aguilarvivaldo29@myhunter.cuny.edu>
2024-09-03 22:41:35 -0400

    Changed background

---
+++ FINAL-Project/script.js
@@ -0,0 +1,42 @@
+
+
+function emmaFunction() {
+    // document.getElementById('wrapper').classList.add('hidden')
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+  }
+
+function isabellaFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function connyFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function dekuFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function tomuraFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function todorokiFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function senkuFunction() {
+    document.getElementById('mainCard').classList.add('hidden');
+    document.getElementById('mainID').classList.remove('hidden');
+}
+
+function tsukasaFunction() {
+    document.getElementById('mainCard').classList.add('hidden')
+    document.getElementById('mainID').classList.remove('hidden')
+}
